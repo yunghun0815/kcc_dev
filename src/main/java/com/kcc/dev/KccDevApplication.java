d1bba67cfcc369d838378c6aa252c79645665b43
Jeong Younghun <Jeong Younghun@DESKTOP-Q182NU8>
2023-04-05 12:39:00 +0900

    init

---
+++ src/main/java/com/kcc/dev/KccDevApplication.java
@@ -0,0 +1,13 @@
+package com.kcc.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KccDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KccDevApplication.class, args);
+	}
+
+}
