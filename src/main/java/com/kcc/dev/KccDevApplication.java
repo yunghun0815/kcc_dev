9828d25b4acaf229887111e4f258824a76d93c9f
Jeong Younghun <Jeong Younghun@DESKTOP-Q182NU8>
2023-04-06 09:29:24 +0900

    젠킨스 테스트

--- src/main/java/com/kcc/dev/KccDevApplication.java
+++ src/main/java/com/kcc/dev/KccDevApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KccDevApplication {
-
+	//test
 	public static void main(String[] args) {
 		SpringApplication.run(KccDevApplication.class, args);
 	}
